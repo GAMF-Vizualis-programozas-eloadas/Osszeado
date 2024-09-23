037e908e96c3c727fbf9ade06d8407c70fc2bfa0
jzscsaba <johanyak.csaba@nje.hu>
2024-09-23 12:14:08 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Osszeado
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btSzámol_Click(object sender, RoutedEventArgs e)
+		{
+			if (!double.TryParse(tbElső.Text, out double a))
+			{
+				MessageBox.Show("Hibás érték az első mezőben!",
+					"Hiba",MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+			if (!double.TryParse(tbMásodik.Text, out double b))
+			{
+				MessageBox.Show("Hibás érték a második mezőben!",
+					"Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+			tbEredmény.Text = (a + b).ToString();
+		}
+
+		private void btKilép_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+	}
+}
